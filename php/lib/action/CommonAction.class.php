ac7eb13d55e8cc9ee4b16bcc36cde138979a4abc
zwb88008 <zwb88008@163.com>
2015-12-21 16:39:31 +0800

    Create CommonAction.class.php
    thinkphp模板 admin/lib/action

---
+++ php/lib/action/CommonAction.class.php
@@ -0,0 +1,9 @@
+<?php
+		class CommonAction extends Action{
+			public function _initialize(){
+				if(!isset($_SESSION['uid'])||!isset($_SESSION['username'])){
+					$this->redirect('Admin/Login/index');
+				}
+			}
+			
+		}
