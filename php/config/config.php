5eb9c68b2a682110c3e5fbb905f65a483d9b79a0
zwb88008 <zwb88008@163.com>
2015-12-21 16:38:06 +0800

    Create config.php
    thinkphp模板 admin/conf/config.php

---
+++ php/config/config.php
@@ -0,0 +1,14 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+	//数据库连接参数
+	'DB_HOST' =>'localhost',
+	'DB_USER' => 'root',
+	'DB_PWD'  => '',
+	'DB_NAME' =>  'ylic',
+	'DB_PREFIX' => 'hd_',
+	//点语法默认解析
+	'TMPL_VAR_IDENTIFY' => 'array',
+	'TMPL_FILE_DEPR' =>'_'
+);
+?>
